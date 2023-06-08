27b3c809ce0251a925b4e7a11a5f0e42bb1c551a
Max <maxkryvytskyiwork@gmail.com>
2023-06-08 21:45:07 +0300

    pass

--- 06_14.py
+++ 06_14.py
@@ -1,3 +1,14 @@
+'''
+Створіть функціонал для розпакування архіву.
+
+Зробіть import пакету shutil
+
+Створіть функцію unpack(archive_path, path_to_unpack), 
+яка викликатиме метод пакета shutil unpack_archive та розпаковуватиме архів archive_path у місце path_to_unpack.
+
+Функція нічого не повертає.
+'''
+
 import shutil
 
 archive_path = r"E:\Git_Files\Mycode\Python\Autocheck\Module_6\folder\backup_folder.zip"
